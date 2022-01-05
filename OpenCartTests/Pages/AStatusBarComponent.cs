7a9e1aa29807f37f1db07f53d94830a3a51397f8
Talion97 <shulgad1997@gmail.com>
2022-01-05 20:46:56 +0200

    Add project files.

---
+++ OpenCartTests/Pages/AStatusBarComponent.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCartTests.Pages
+{
+    public abstract class AStatusBarComponent : AHeadComponent
+    {
+        // fields
+
+        public AStatusBarComponent(IWebDriver driver) : base(driver)
+        {
+            // constructor
+        }
+
+        // Methods
+    }
+}
---
+++ OpenCartTests/Tests/Shulga Dmytro/FeedbackTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace OpenCartTests
+{
+    // TODO
+    [TestFixture]
+    public class FeedbackTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
