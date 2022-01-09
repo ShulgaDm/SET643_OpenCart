55d75d4ada297faa6bd1e0edf2e0d4ee58ade638
CristinaBud <96821295+CristinaBud@users.noreply.github.com>
2022-01-09 21:10:01 +0200

    Search result page: (#6)
    home page
    head component
    seach criteria component
    search result page

---
+++ OpenCartTests/Tests/Cristina Budzinska/SearchPageTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenCartTests.Pages;
+using OpenCartTests.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCartTests.Tests.Cristina_Budzinska
+{
+    [TestFixture]
+    public class SearchPageTest: TestRunner 
+    {
+        protected override string OpenCartURL { get => "http://opencart"; }
+        [Test]
+        public void SearchResultPageTest()
+        {
+            string expectedResult = "Search - iPhone";
+
+            HomePage homePage = new HomePage(driver);
+            SearchResultPage resultPage = (SearchResultPage)homePage.FindProduct("iPhone");
+
+            string actualResult = resultPage.ResultPageHeader.Text;
+            Assert.AreEqual(expectedResult, actualResult);   //check if page header equals expected result
+        }
+    }
+}
