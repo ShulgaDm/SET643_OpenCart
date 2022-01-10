10058aaa35c68dfb7206482feec3616420b35ea3
Nazar Dykyy <78155745+bestmertnyy@users.noreply.github.com>
2022-01-10 19:13:56 +0200

    Closes #8 - EmptyWishListAfterFirstLogin (#9)

---
+++ OpenCartTests/Pages/WishListPage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCartTests.Pages
+{
+    public class WishListPage : ARightMenuComponent
+    {
+        public IWebElement VerifyWishList { get; private set; }
+        public WishListPage(IWebDriver driver) : base(driver)
+        {
+            VerifyWishList = driver.FindElement(By.CssSelector (".container .row .col-sm-9 p"));
+        }
+
+        public string GetVerifyWishListText() => VerifyWishList.Text;
+        /*public void ClickNewWishListButton() => NewWishList.Click();
+        public string GetNewWishListButtonText() => NewWishList.Text;*/
+
+        public void VerifyWishListPage()
+        {
+            GetVerifyWishListText();
+        }
+    }
+}
