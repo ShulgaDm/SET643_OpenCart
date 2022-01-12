e7e80deb507f19fcaa392491c77288b0ac95dd46
bohdan-khudio <bahdan510@gmail.com>
2022-01-12 18:27:58 +0200

    upd

--- OpenCartTests/Tests/Bohdan Khudo/ChangePasswordTest.cs
+++ OpenCartTests/Tests/Bohdan Khudo/ChangePasswordTest.cs
@@ -3,28 +3,34 @@ using OpenCartTests.Data;
 using OpenCartTests.Pages;
 using OpenCartTests.Tools;
 
-namespace OpenCartTests.Tests.Bohdan_Khudo;
-
-[TestFixture]
-public class ChangePasswordTest: TestRunner
+namespace OpenCartTests.Tests.Bohdan_Khudo
 {
-    protected override string OpenCartURL { get => "http://localhost/opencart3/upload"; }
-    [Test]
-    public void ChangePassword()
+
+    [TestFixture]
+    public class ChangePasswordTest : TestRunner
     {
-        User user = User.CreateBuilder().SetEMail("bahdan510@gmail.com").SetPassword("school22").Build();
+        protected override string OpenCartURL
+        {
+            get => "http://localhost/opencart3/upload";
+        }
 
-        new HomePage(driver)
-            .GoToLoginPage()
-            .SuccessfullLogin(user)
-            .GoToMyAccountPage()
-            .ClickhangeYourPassword();
-        
+        [Test]
+        public void ChangePassword()
+        {
+            User user = User.CreateBuilder().SetEMail("bahdan510@gmail.com").SetPassword("school22").Build();
 
-        ChangePasswordPage changePasswordPage = new ChangePasswordPage(driver);
-        changePasswordPage.PasswordField.SendKeys("school33");
-        changePasswordPage.PasswordConfirm.SendKeys("school33");
-        changePasswordPage.ClickContinueButton();
+            new HomePage(driver)
+                .GoToLoginPage()
+                .SuccessfullLogin(user)
+                .GoToMyAccountPage()
+                .ClickhangeYourPassword();
 
+
+            ChangePasswordPage changePasswordPage = new ChangePasswordPage(driver);
+            changePasswordPage.PasswordField.SendKeys("school33");
+            changePasswordPage.PasswordConfirm.SendKeys("school33");
+            changePasswordPage.ClickContinueButton();
+
+        }
     }
 }
