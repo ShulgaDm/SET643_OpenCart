dbcbd064734691bd975d3fe611a48229e17c7a31
Talion97 <shulgad1997@gmail.com>
2022-01-14 00:55:23 +0200

    Check Product Reviews
    Changed:
    HomePage
    
    Added:
    Review & ReviewBuilder
    ProductDetailsPage & ProductDetailsMessagePage

--- OpenCartTests/Tests/Shulga Dmytro/FeedbackTests.cs
+++ OpenCartTests/Tests/Shulga Dmytro/FeedbackTests.cs
@@ -1,22 +1,54 @@
 using NUnit.Framework;
+using OpenCartTests.Data;
+using OpenCartTests.Pages;
 using OpenCartTests.Tools;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using System;
-using System.Threading;
 
 namespace OpenCartTests.Tests.Shulga_Dmytro
 {
-    // TODO
     [TestFixture]
     public class FeedbackTests : TestRunner
     {
         protected override string OpenCartURL { get => "http://cart"; }
+        public readonly string EXPECTED_NO_REVIEWS_MESSAGE = "There are no reviews for this product.";
+        public readonly string EXPECTED_SUCCESSFULL_ADD_REVIEW_MESSAGE =
+                    "Thank you for your review. It has been submitted to the webmaster for approval.";
+
+        Review review;
+
+        [OneTimeSetUp]
+        public void BeforeAllMethods()
+        {
+            review = Review.CreateBuilder()
+                        .SetYourName("Ivan")
+                        .SetYourReview("It is the best product in the world")
+                        .SetRatingValue("3").Build();
+        }
 
         [Test]
-        public void Test1()
+        public void CheckProductWithEmptyReview()
         {
-            Assert.Pass();
+            string expected = EXPECTED_NO_REVIEWS_MESSAGE;
+
+            string actual = new HomePage(driver)
+                                .GetFirstProductDetails()
+                                .OpenReviews()
+                                .GetReviewText();
+
+            Assert.IsTrue(actual.Contains(expected));
+        }
+
+        [Test]
+        public void SuccessfullAddReviewTest()
+        {
+            string expected = EXPECTED_SUCCESSFULL_ADD_REVIEW_MESSAGE;
+
+            string actual = new HomePage(driver)
+                                .GetFirstProductDetails()
+                                .OpenReviews()
+                                .SuccessfullAddReview(review)
+                                .GetAlertMessageText();
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
