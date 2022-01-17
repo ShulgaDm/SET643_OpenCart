3d2cc1a3fcf0e5bec134a4b657aeca4e258025e3
Anastasiia-Rokytska <55663939+Anastasiia-Rokytska@users.noreply.github.com>
2022-01-17 15:47:23 +0200

    Closes #37 - Authorized User can view Empty Shopping Cart (#41)
    * Closes #37 - Authorized User can view Empty Shopping Cart

--- OpenCartTests/Tests/Anastasiia Rokytska/ShoppingCartTests.cs
+++ OpenCartTests/Tests/Anastasiia Rokytska/ShoppingCartTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using NUnit.Framework;
 using OpenCartTests.Data;
 using OpenCartTests.Pages;
@@ -16,11 +17,39 @@ namespace OpenCartTests.Tests.Anastasiia_Rokytska
         //protected override string OpenCartURL { get => "http://localhost/index.php?route=checkout/cart"; }
         private readonly string EMPTY_SHOPPING_CART_TEXT = "Your shopping cart is empty!";
         protected override string OpenCartURL { get => "http://localhost"; }
+
+        User user;
+
+        [OneTimeSetUp]
+        public void BeforeAllMethods()
+        {
+            user = User.CreateBuilder()
+              .SetFirstName("test")
+              .SetLastName("test")
+              .SetEMail("test@gmail.com")
+              .SetTelephone("0673256485")
+              .SetPassword("test")
+              .Build();
+        }
         [Test]
         public void EmptyShoppingCartWithoutLogging()
         {
             ShoppingCartPage shoppingCartPage = new HomePage(driver).GoToShoppingCartPage();
+            // TODO check whether user is authorized
             string actualResult = shoppingCartPage.GetEmptyShoppingCartText();
+            Assert.AreEqual(EMPTY_SHOPPING_CART_TEXT, actualResult);
+        }
+
+        [Test]
+        public void EmptyShoppingCartWithLogging()
+        {
+            RegisterPage registerPage = new HomePage(driver).GoToRegisterPage();
+            registerPage.FillRegisterForm(user);
+            registerPage.ClickAgreeCheckBox();
+            AccountSuccessPage successPage = registerPage.ClickContinueButtonSuccess();
+            // TODO check whether user is authorized
+            Thread.Sleep(1000);
+            string actualResult = new HomePage(driver).GoToShoppingCartPage().GetEmptyShoppingCartText();
             Assert.AreEqual(EMPTY_SHOPPING_CART_TEXT, actualResult);
         }
 
