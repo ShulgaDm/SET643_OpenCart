d557c15d2b4e46e1d0035d3c727ef72392fe9be7
Yura Kanafotskyi <57145895+kanafka16@users.noreply.github.com>
2022-01-18 09:58:22 +0200

    Closes #50 - Price changes depending on currency (wish list) (#51)
    Co-authored-by: kanafka16 <yurii.kanafotskyi@gmail.com>

--- OpenCartTests/Pages/WishListPage.cs
+++ OpenCartTests/Pages/WishListPage.cs
@@ -13,10 +13,10 @@ namespace OpenCartTests.Pages
     public class WishListPage : AStatusBarComponent 
     {        
         public IWebElement DeleteButton { get {return driver.FindElement(By.CssSelector("#content tr:first-child .btn-danger")); } }
-        
         public IWebElement Alertmessage { get { return driver.FindElement(By.CssSelector(".alert-success:not( .fa-check-circle)")); } }
         public IWebElement EmptyWishListMessage { get { return driver.FindElement(By.CssSelector("#content > p")); } }
         public IWebElement Table { get { return driver.FindElement(By.CssSelector("#content > div.table-responsive")); } }
+        public IWebElement WishListFirstProductPrice { get { return driver.FindElement(By.CssSelector("tr:first-child .price")); } }
 
         public WishListPage(IWebDriver driver) : base(driver)
         {           
@@ -41,6 +41,10 @@ namespace OpenCartTests.Pages
         public string GetEmptyWishListMessageText()
         {
             return EmptyWishListMessage.Text;
+        }
+        public string GetWishListFirstProductPrice()
+        {
+            return WishListFirstProductPrice.Text;
         }
     }
 }
